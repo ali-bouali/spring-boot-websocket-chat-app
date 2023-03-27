3227778f424814f1dae69376864cf0a0356e5615
ali-bouali <bouali.pro@gmail.com>
2023-03-27 14:43:51 +0200

    initializing the project

---
+++ src/test/java/com/alibou/websocket/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
