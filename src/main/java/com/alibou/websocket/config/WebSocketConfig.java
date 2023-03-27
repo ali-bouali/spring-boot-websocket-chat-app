206d8d4be0e905ed44d3127fdfcff2e92a90cc22
ali-bouali <bouali.pro@gmail.com>
2023-03-27 14:44:11 +0200

    adds websocket config

---
+++ src/main/java/com/alibou/websocket/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.alibou.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
