3756b3bdc873c511587f04a9647530177087afdb
ali-bouali <bouali.pro@gmail.com>
2023-05-19 15:10:02 +0200

    upgrading the UI

--- src/main/java/com/alibou/websocket/config/WebSocketConfig.java
+++ src/main/java/com/alibou/websocket/config/WebSocketConfig.java
@@ -2,7 +2,9 @@ package com.alibou.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
