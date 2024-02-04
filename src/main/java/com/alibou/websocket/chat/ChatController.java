c20c36f2ebb24a6834447a665b84715408512165
devtam-02 <nguyentrongtam2x2@gmail.com>
2024-02-04 19:34:39 +0700

    add crossOrigin

--- src/main/java/com/alibou/websocket/chat/ChatController.java
+++ src/main/java/com/alibou/websocket/chat/ChatController.java
@@ -5,8 +5,10 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class ChatController {
 
     @MessageMapping("/chat.sendMessage")
