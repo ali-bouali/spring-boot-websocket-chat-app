420f73e76f36dc98955d2664000c90085b72de0b
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-05-23 14:24:07 +0530

    Excluding STOMP:  - Pre existing stomp configuration has been removed  - New configuration with the help of basic Spring WebSocket API  - MyHandler.java which handles the different events during connection

---
+++ src/main/java/com/alibou/websocket/config/MyHandler.java
@@ -0,0 +1,8 @@
+package com.alibou.websocket.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class MyHandler extends TextWebSocketHandler {
+}
