8af90b67c21b798d6e2a35264caa08d4eb82625f
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-05-23 15:43:31 +0530

    Login Feature:  - Previous libraries AJAX, Stomp has been removed  - Instead of them native WebSocket class has been used  - Unnecessary functions are removed and onMessageReceived, onError has been mapped to corresponding socket method

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -10,7 +10,7 @@ window.onload = () => {
     const messageArea = document.querySelector('#messageArea');
     const connectingElement = document.querySelector('.connecting');
 
-    let stompClient = null;
+    let socket = null;
     let username = null;
 
     const colors = [
@@ -25,28 +25,15 @@ window.onload = () => {
             usernamePage.classList.add('hidden');
             chatPage.classList.remove('hidden');
 
-            const socket = new SockJS('/ws');
-            stompClient = Stomp.over(socket);
+            socket = new WebSocket(`ws://localhost:8080/chat?username=${username}`)// connection established
+            socket.onmessage = onMessageReceived
+            socket.onerror = onError
 
-            stompClient.connect({}, onConnected, onError);
+            connectingElement.classList.add('hidden');
         }
+
         event.preventDefault();
     }
-
-
-    function onConnected() {
-        // Subscribe to the Public Topic
-        stompClient.subscribe('/topic/public', onMessageReceived);
-
-        // Tell your username to the server
-        stompClient.send("/app/chat.addUser",
-            {},
-            JSON.stringify({sender: username, type: 'JOIN'})
-        )
-
-        connectingElement.classList.add('hidden');
-    }
-
 
     function onError(error) {
         connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
