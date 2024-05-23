e23ac2f0848c89bfb3e7eb378d2febae2a977c1c
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-05-23 15:04:54 +0530

    Login Feature:  - Pre existing controller has been removed  - Now login is implemented by overriding afterConnectionEstablished method.  - The above method is compatible for token based authentication.  - Instead of broker a List of WebSocketSession has been used to keep connected sessions.

--- src/main/java/com/alibou/websocket/config/MyHandler.java
+++ src/main/java/com/alibou/websocket/config/MyHandler.java
@@ -1,8 +1,51 @@
 package com.alibou.websocket.config;
 
+import com.alibou.websocket.chat.ChatMessage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.alibou.websocket.chat.MessageType.JOIN;
+
 @Component
+@Slf4j
 public class MyHandler extends TextWebSocketHandler {
+    private List<WebSocketSession> sessions = new ArrayList<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        // If not existing session add it.
+        if (!sessions.contains(session)) {
+            log.info("New Session Connected sessionId: {}", session.getId());
+            sessions.add(session);
+
+            /*
+             *  The objective of following this approach here is instead of passing username
+             *  actual Authorization token can be passed. During login the token can be further
+             *  obtained from a controller. The implementation should be different for both opaque token
+             *  and JWT.
+             */
+            String query = session.getUri().getQuery(); // extract username from query param
+            String username = query.substring(9); // query will look something like this "username=some-username"
+
+            session.getAttributes().put("username", username); // set the username in session
+
+            ChatMessage chatMessage = ChatMessage.builder()
+                    .sender(username)
+                    .content(username + " has joined.")
+                    .type(JOIN)
+                    .build();
+
+            String chatMessageAsJson = objectMapper.writeValueAsString(chatMessage);
+
+            session.sendMessage(new TextMessage(chatMessageAsJson));
+        }
+    }
 }
